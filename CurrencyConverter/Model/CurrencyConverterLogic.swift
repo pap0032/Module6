0a67ebd8311e09e197aa399ffa474a48860b1434
Patrick Parks <patrickparks@Patricks-MacBook-Pro.local>
2024-11-24 22:48:55 -0500

    Initial Commit

--- CurrencyConverter/Model/CurrencyConverterLogic.swift
+++ CurrencyConverter/Model/CurrencyConverterLogic.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class CurrencyConverter {
+    // Dictionary to store conversion rates (example rates)
+    private let conversionRates: [String: Double] = [
+        "GBP": 1.2629, // USD to Currency 1
+        "Euros": 1.0552, // USD to Currency 2
+        "Yen": 154.59, // USD to Currency 3
+        "Franc": 0.89 // USD to Currency 4
+    ]
+    
+    // Function to convert USD to a specific currency
+    func convert(amountInUSD: Int, to currency: String) -> Double? {
+        guard let rate = conversionRates[currency] else { return nil }
+        return Double(amountInUSD) * rate
+    }
+    
+    // Function to convert USD to multiple selected currencies
+    func convert(amountInUSD: Int, for selectedCurrencies: [String: Bool]) -> [String: Double] {
+        var convertedAmounts: [String: Double] = [:]
+        
+        for (currency, isSelected) in selectedCurrencies {
+            if isSelected, let convertedAmount = convert(amountInUSD: amountInUSD, to: currency) {
+                convertedAmounts[currency] = convertedAmount
+            }
+        }
+        
+        return convertedAmounts
+    }
+}
--- CurrencyConverter/View/ConvertedValuesViewController.swift
+++ CurrencyConverter/View/ConvertedValuesViewController.swift
@@ -16,36 +16,39 @@ class ConvertedValuesViewController: UIViewController {
     
     var usdAmount: Int?
     var selectedCurrencies: [String: Bool] = [:]
+    private let currencyConverter = CurrencyConverter()
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        guard let usd = usdAmount else { return }
-        
-        usdLabel.text = "Amount in USD: \(usd)"
-        
-        let conversionRates = [
-            "GBP": 1.2629,
-            "Euros": 1.0552,
-            "Yen": 154.59,
-            "Franc": 0.89
-        ]
-        
-        poundsLabel.isHidden = !selectedCurrencies["GBP"]!
-        euroLabel.isHidden = !selectedCurrencies["Euros"]!
-        yenLabel.isHidden = !selectedCurrencies["Yen"]!
-        francLabel.isHidden = !selectedCurrencies["Franc"]!
-        
-        if selectedCurrencies["GBP"]! {
-            poundsLabel.text = "GBP: \(Double(usd) * conversionRates["GBP"]!)"
-        }
-        if selectedCurrencies["Euros"]! {
-            euroLabel.text = "Euros: \(Double(usd) * conversionRates["Euros"]!)"
-        }
-        if selectedCurrencies["Yen"]! {
-            yenLabel.text = "Yen: \(Double(usd) * conversionRates["Yen"]!)"
-        }
-        if selectedCurrencies["Franc"]! {
-            francLabel.text = "Franc: \(Double(usd) * conversionRates["Franc"]!)"
+            super.viewDidLoad()
+            guard let usdAmount = usdAmount else { return }
+            usdLabel.text = "Amount in USD: \(usdAmount)"
+            
+            // Use the CurrencyConverter to calculate the amounts
+            let convertedAmounts = currencyConverter.convert(amountInUSD: usdAmount, for: selectedCurrencies)
+            
+            // Update labels based on selected currencies
+            if selectedCurrencies["GBP", default: false] {
+                poundsLabel.text = "GBP: \(convertedAmounts["GBP"] ?? 0.0)"
+            } else {
+                poundsLabel.isHidden = true
+            }
+            
+            if selectedCurrencies["Euros", default: false] {
+                euroLabel.text = "Euros: \(convertedAmounts["Euros"] ?? 0.0)"
+            } else {
+                euroLabel.isHidden = true
+            }
+            
+            if selectedCurrencies["Yen", default: false] {
+                yenLabel.text = "Yen: \(convertedAmounts["Yen"] ?? 0.0)"
+            } else {
+                yenLabel.isHidden = true
+            }
+            
+            if selectedCurrencies["Franc", default: false] {
+                francLabel.text = "Franc: \(convertedAmounts["Franc"] ?? 0.0)"
+            } else {
+                francLabel.isHidden = true
+            }
         }
     }
-}
